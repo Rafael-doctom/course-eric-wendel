d1e4597d62311f5c9afc1f482e765e4f97474f1d
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-12-01 18:33:27 -0300

    calback tasks

---
+++ codigo1/index.js
@@ -0,0 +1,41 @@
+// obter user
+// pegar o numero à partir do ID
+// Obter adress à partir do ID
+
+// function getUser(){
+//     setTimeout(() => {
+//         return{
+//             id: 1,
+//             nome:'Rafael',
+//             date: new Date()
+//         }
+//     }, 1000)
+// }
+
+// const user = getUser()
+// console.log('usuário', user)
+
+
+// const meuNome = (nome) => {
+//     return `Meu nome é ${nome}`
+// }
+
+// const pegarNome = (calback) => {
+//     let nome = 'rafa'
+//     console.log(calback(nome))
+// }
+
+// pegarNome(meuNome)
+
+// const task1 = () => {
+//     console.log('task number one')
+// }
+
+// const task2 = () => {
+//     setTimeout(() => {
+//         console.log('task number two')
+//     }, 3000)
+// }
+
+// task2()
+// task1()
